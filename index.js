3be99f317212ed940037e7704c8c99fdabe81235
Wanderson Castro <wandersonce1@hotmail.com>
2020-07-28 11:55:26 -0700

    Login in function

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const ig = require('./instagram');
 (async () => {
     await ig.initialize();
 
+    await ig.login('bamgamesofc', 'f1a6r2a7bam');
+
     debugger;
 
 })();
--- instagram.js
+++ instagram.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
 
-const BASE_URL = 'https://instagram.com/'
+const BASE_URL = 'https://www.instagram.com/'
 
 const instagram = {
     browser: null,
     page: null,
 
+    //* Initializing  the browser and going to instagram home page
     initialize: async () => {
         instagram.browser = await puppeteer.launch({
             headless: false
@@ -13,7 +14,23 @@ const instagram = {
 
         instagram.page = await instagram.browser.newPage();
 
+    },
+
+    //* Login to user account
+    login: async (username, password) => {
         await instagram.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+
+        await instagram.page.waitFor(1000);
+
+        /* Writing the username and password */
+        await instagram.page.type('input[name="username"]', username, { delay: 50 });
+        await instagram.page.type('input[name="password"]', password, { delay: 50 });
+
+        /* Click on the login url button */
+        let loginButton = await instagram.page.$x('//div[contains(text(), "Log In")]');
+        await loginButton[0].click();
+
+        debugger;
     }
 }
 
