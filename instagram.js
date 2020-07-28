dcaf852edbc37a1c50528bcd44f713d7d127f402
Wanderson Castro <wandersonce1@hotmail.com>
2020-07-28 11:14:17 -0700

    First test using puppeteer to open instagram

---
+++ instagram.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+const BASE_URL = 'https://instagram.com/'
+
+const instagram = {
+    browser: null,
+    page: null,
+
+    initialize: async () => {
+        instagram.browser = await puppeteer.launch({
+            headless: false
+        });
+
+        instagram.page = await instagram.browser.newPage();
+
+        await instagram.page.goto(BASE_URL, { waitUntil: 'networkidle2' });
+    }
+}
+
+module.exports = instagram;
